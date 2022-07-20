b1f931009d3087de4d35b1e8d20dc628b71edf50
shreyashdj <shreyashjadhav1799@gmail.com>
2022-07-20 07:44:12 +0530

    [Shreyash]: added program to calculate Emp daily wage

--- src/com/bridgelabz/day3EmpWage/EmployeeWage.java
+++ src/com/bridgelabz/day3EmpWage/EmployeeWage.java
@@ -6,16 +6,24 @@ public class EmployeeWage {
 
 		System.out.println("Welcome to Employee Wage Calculation");
 		
+		int wagePerHour = 20;
+		int fullDayHours = 8;
+		
 		 int attendance = (int) (Math.random()*2);		// used random function to get output (0 or 1)
 	     
 		 /*
-		  * 	applied if-else condition to check employee attendance
+		  * 	applied if-else condition to check employee attendance and
+		  * 	calculated employee daily wage
 		  */
 		 
 	        if (attendance == 0) {
 	            System.out.println("Emloyee is Absent");
-	       } else
+	            System.out.println("Daily Employee Wage = 0");
+	       } else {
 	            System.out.println("Employee is Present");
+	        	int dailyWage = wagePerHour * fullDayHours;
+	        	System.out.println("Daily Employee Wage = " +dailyWage);
+	       }	
 		
 	}
 
