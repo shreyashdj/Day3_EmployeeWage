3064c7ef3533c2a02b1c7b49e294d2107111489c
shreyashdj <shreyashjadhav1799@gmail.com>
2022-07-20 11:42:30 +0530

    [Shreyash]: added program to calculate employee monthly wage

--- src/com/bridgelabz/day3EmpWage/EmployeeWage.java
+++ src/com/bridgelabz/day3EmpWage/EmployeeWage.java
@@ -6,38 +6,48 @@ public class EmployeeWage {
 
 		System.out.println("Welcome to Employee Wage Calculation");
 		
-		int wagePerHour = 20;
-		int fullDayHours = 8;
-		int partTimeHours = 4;
+		final int workingDayInMonth = 20;
+		final int wagePerHour = 20;
+		final int fullDayHours = 8;
+		final int partTimeHours = 4;
+		int day = 0;
+		int totalEmpHours = 0;
+		
+		 /*
+		  * 	used while loop till 20 working days
+		  * 	used switch case to
+		  * 	check employee attendance.
+		  * 	calculated employee monthly wage
+		  */
+		
+		while (day < workingDayInMonth) {				
+				day++;
 		
 		 int attendance = (int) (Math.random()*3);		// used random function to get output (0/1/2)
-	     
-		 /*
-		  * 	applied if-else condition to check employee attendance and
-		  * 	calculated employee daily wage
-		  * 	added part time employee & their wage
-		  * 	used switch case
-		  */
 		 
 		 switch (attendance) {
 		 
 		    case 0 : 
-		    		System.out.println("Emloyee is Absent");
-		    		System.out.println("Daily Employee Wage = 0");
-		    	break;
+		    		System.out.println("\n Day: " + day);
+					System.out.println("Employee is Absent & Emp Hour : 0");
+		      break;
 		    	
 		    case 1 :	        
-	        	int dailyWage = wagePerHour * fullDayHours;
-	            	System.out.println("Employee is Present");	        	
-	            	System.out.println("Daily Employee Wage = " +dailyWage);
-	        	break;
+			    	 totalEmpHours += fullDayHours;	
+					System.out.println("\n Day: " + day);
+					System.out.println("Employee is Present & Emp Hour : " + fullDayHours);
+	          break;
 	        	
 	       default :
-	        	 int dailyWage1 = wagePerHour * partTimeHours;
-	        	 System.out.println("Employee is Part Time");
-	        	 System.out.println("Daily Employee Wage = " +dailyWage1);	        
+		    	    totalEmpHours += partTimeHours;	
+					System.out.println("\n Day: " + day);
+					System.out.println("Employee is Part Time & Emp Hour : " + partTimeHours);     
 			}
-	        
+
+		}
+		 int totalEmpWage = totalEmpHours * wagePerHour;
+		 System.out.println("\n Total Employee Hours = " + totalEmpHours);
+		 System.out.println(" Monthly Employee Wage = " + totalEmpWage);
 	}
 
 }
