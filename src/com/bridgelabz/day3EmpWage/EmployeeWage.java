134c29df3f92ee46c5994126fe6d7a8f2943a539
shreyashdj <shreyashjadhav1799@gmail.com>
2022-07-20 07:27:35 +0530

    [Shreyash]: added program to check employee attendance

--- src/com/bridgelabz/day3EmpWage/EmployeeWage.java
+++ src/com/bridgelabz/day3EmpWage/EmployeeWage.java
@@ -6,6 +6,17 @@ public class EmployeeWage {
 
 		System.out.println("Welcome to Employee Wage Calculation");
 		
+		 int attendance = (int) (Math.random()*2);		// used random function to get output (0 or 1)
+	     
+		 /*
+		  * 	applied if-else condition to check employee attendance
+		  */
+		 
+	        if (attendance == 0) {
+	            System.out.println("Emloyee is Absent");
+	       } else
+	            System.out.println("Employee is Present");
+		
 	}
 
 }
