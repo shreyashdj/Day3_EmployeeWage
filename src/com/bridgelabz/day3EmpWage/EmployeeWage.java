0dc7dc93b9ca97e3c28bb0b35f1f51ac1784209a
shreyashdj <shreyashjadhav1799@gmail.com>
2022-07-20 07:57:06 +0530

    [Shreyash]: added program to add part time Emp & their wage

--- src/com/bridgelabz/day3EmpWage/EmployeeWage.java
+++ src/com/bridgelabz/day3EmpWage/EmployeeWage.java
@@ -8,23 +8,31 @@ public class EmployeeWage {
 		
 		int wagePerHour = 20;
 		int fullDayHours = 8;
+		int partTimeHours = 4;
 		
-		 int attendance = (int) (Math.random()*2);		// used random function to get output (0 or 1)
+		 int attendance = (int) (Math.random()*3);		// used random function to get output (0/1/2)
 	     
 		 /*
 		  * 	applied if-else condition to check employee attendance and
 		  * 	calculated employee daily wage
+		  * 	added part time employee & their wage
 		  */
 		 
 	        if (attendance == 0) {
 	            System.out.println("Emloyee is Absent");
 	            System.out.println("Daily Employee Wage = 0");
-	       } else {
-	            System.out.println("Employee is Present");
+	       }
+	        else if (attendance == 1) {
 	        	int dailyWage = wagePerHour * fullDayHours;
+	            System.out.println("Employee is Present");	        	
 	        	System.out.println("Daily Employee Wage = " +dailyWage);
-	       }	
-		
+	       }
+	       else {
+	        	 int dailyWage = wagePerHour * partTimeHours;
+	        	 System.out.println("Employee is Part Time");
+	        	 System.out.println("Daily Employee Wage = " +dailyWage);	        
+	       }
+	        
 	}
 
 }
