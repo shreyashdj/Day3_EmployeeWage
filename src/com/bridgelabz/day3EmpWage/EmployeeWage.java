aea938ebb66e82aa394c14a3f9e2b95157c0ae4a
shreyashdj <shreyashjadhav1799@gmail.com>
2022-07-20 08:08:39 +0530

    [Shreyash]: modified program to solve using Switch case

--- src/com/bridgelabz/day3EmpWage/EmployeeWage.java
+++ src/com/bridgelabz/day3EmpWage/EmployeeWage.java
@@ -16,22 +16,27 @@ public class EmployeeWage {
 		  * 	applied if-else condition to check employee attendance and
 		  * 	calculated employee daily wage
 		  * 	added part time employee & their wage
+		  * 	used switch case
 		  */
 		 
-	        if (attendance == 0) {
-	            System.out.println("Emloyee is Absent");
-	            System.out.println("Daily Employee Wage = 0");
-	       }
-	        else if (attendance == 1) {
+		 switch (attendance) {
+		 
+		    case 0 : 
+		    		System.out.println("Emloyee is Absent");
+		    		System.out.println("Daily Employee Wage = 0");
+		    	break;
+		    	
+		    case 1 :	        
 	        	int dailyWage = wagePerHour * fullDayHours;
-	            System.out.println("Employee is Present");	        	
-	        	System.out.println("Daily Employee Wage = " +dailyWage);
-	       }
-	       else {
-	        	 int dailyWage = wagePerHour * partTimeHours;
+	            	System.out.println("Employee is Present");	        	
+	            	System.out.println("Daily Employee Wage = " +dailyWage);
+	        	break;
+	        	
+	       default :
+	        	 int dailyWage1 = wagePerHour * partTimeHours;
 	        	 System.out.println("Employee is Part Time");
-	        	 System.out.println("Daily Employee Wage = " +dailyWage);	        
-	       }
+	        	 System.out.println("Daily Employee Wage = " +dailyWage1);	        
+			}
 	        
 	}
 
